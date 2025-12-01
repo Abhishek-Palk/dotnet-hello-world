cb2ccfbd2fd6d6d30731cfbb49400b0bd9f9bbd8
Abhishek-Palk <apalkar469@gmail.com>
2025-12-02 01:00:37 +0530

    Update Startup.cs

--- hello-world-api/Startup.cs
+++ hello-world-api/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -27,7 +28,7 @@ namespace hello_world_api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -38,3 +39,4 @@ namespace hello_world_api
         }
     }
 }
+
